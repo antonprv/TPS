a8d01a9f124d7425cd00504a0771aea90c42a681
anton.prv <anton.prv@yandex.com>
2025-08-23 23:17:11 +0400

    Githooks: test 2

--- Source/TPS/TPSCharacter.cpp
+++ Source/TPS/TPSCharacter.cpp
@@ -85,7 +85,6 @@ void ATPSCharacter::OnResetVR()
 void ATPSCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
 {
             Jump();
-
 }
 
 void ATPSCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
